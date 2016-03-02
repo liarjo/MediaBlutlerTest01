f8a674301fabd2fc10bb49fcff78365a4bb89cbe
liarjo <jpgarcia69@hotmail.com>
2016-03-02 13:28:51 -0500

    Change all Encodig preset files to support MEdia Encodr Standar

--- MediaButler.BaseProcess/Base/StandarEncodeStep.cs
+++ MediaButler.BaseProcess/Base/StandarEncodeStep.cs
@@ -25,48 +25,9 @@ namespace MediaButler.BaseProcess
         private IEncoderSupport myEncodigSupport;
         private IButlerStorageManager myStorageManager;
         /// <summary>
-        /// Load encoding profile from local disk or Blob Storage (mediabultlerbin container)
+        /// Load Encoding profile definition for 
         /// </summary>
-        /// <param name="profileInfo"></param>
         /// <returns></returns>
-        private string LoadEncodeProfile(string profileInfo)
-        {
-            string auxProfile = null;
-            if (File.Exists(Path.GetFullPath(@".\encodingdefinitions\" + profileInfo)))
-            {
-                //the XML file is in the local file
-                Trace.TraceInformation("[{0}] process Type {1} instance {2} Encoder Profile {3} from local disk", this.GetType().FullName, myRequest.ProcessTypeId, myRequest.ProcessInstanceId, profileInfo);
-                auxProfile = File.ReadAllText(Path.GetFullPath(@".\encodingdefinitions\" + profileInfo));
-            }
-            else
-            {
-                //The profile is not in local file
-
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(myRequest.ProcessConfigConn);
-                CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                CloudBlobContainer container = blobClient.GetContainerReference(MediaButler.Common.Configuration.ButlerExternalInfoContainer);
-                CloudBlockBlob blockBlob = container.GetBlockBlobReference(profileInfo);
-                if (blockBlob.Exists())
-                {
-                    Trace.TraceInformation("[{0}] process Type {1} instance {2} Encoder Profile {3} from Blob Storage", this.GetType().FullName, myRequest.ProcessTypeId, myRequest.ProcessInstanceId, profileInfo);
-         
-                     using (var memoryStream = new MemoryStream())
-                    {
-                        blockBlob.DownloadToStream(memoryStream);
-                        memoryStream.Position = 0;
-                        StreamReader sr = new StreamReader(memoryStream);
-                        auxProfile = sr.ReadToEnd();
-                    }
-                }
-                else
-                {
-                    string txtMessage = string.Format("[{0}] process Type {1} instance {2} Error Encoder Profile {3} don't exist", this.GetType().FullName, myRequest.ProcessTypeId, myRequest.ProcessInstanceId, profileInfo);
-                    throw (new Exception(txtMessage));
-                }
-                
-            }
-            return auxProfile;
-        }
         private string[] getEncodeInformation()
         {
             
@@ -116,7 +77,7 @@ namespace MediaButler.BaseProcess
                 }
                 else
                 {
-                    encodeProfileName = "H264 Adaptive Bitrate MP4 Set 1080p.xml";
+                    encodeProfileName = "H264 Multiple Bitrate 1080p.json";
                 }
 
                 // xmlEncodeProfile = LoadEncodeProfile(encodeProfileName);
@@ -128,7 +89,8 @@ namespace MediaButler.BaseProcess
         private  void  ConvertMP4toSmooth(IAsset assetToConvert)
         {
             // Declare a new job to contain the tasks
-            currentJob = _MediaServicesContext.Jobs.Create("Convert to Smooth Streaming job " + myAssetOriginal.Name);
+            string jobName = string.Format("Convert to Smooth Streaming job {0} [{1}]",myAssetOriginal.Name,myRequest.ProcessInstanceId);
+            currentJob = _MediaServicesContext.Jobs.Create(jobName);
             // Set up the first Task to convert from MP4 to Smooth Streaming. 
             // Read in task configuration XML
             var encodeData = getEncodeInformation();
@@ -195,12 +157,7 @@ namespace MediaButler.BaseProcess
             }
             catch (Exception)
             {
-
-                
             }
-            
-
-
             return (currentJob == null);
         }
         public override void HandleExecute(Common.workflow.ChainRequest request)
