74640431d49f45148bf222a4ad5e97a5f27bd91c
jpgarcia <jpgarcia@microsoft.com>
2016-11-15 11:19:13 -0500

    Add Azure Search to MBF

---
+++ MediaButler.BaseProcess/AzureSearch/videotxt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaButler.BaseProcess.AzureSearch
+{
+    public class videotext
+    {
+        public string id { get; set; }
+        public string assetid { get; set; }
+        public string title { get; set; }
+        public double recognizability { get; set; }
+        public string begin { get; set; }
+        public string end { get; set; }
+        public string text { get; set; }
+        public string url { get; set; }
+    }
+}
