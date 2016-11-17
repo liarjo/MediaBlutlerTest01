27c372dfcaa148b70df54d77c134b00e49e07f85
jpgarcia <jpgarcia@microsoft.com>
2016-11-17 14:54:14 -0500

    Fix  bug: unpublished program

--- MediaButler.BaseProcess/AzureSearch/InjectTTML.cs
+++ MediaButler.BaseProcess/AzureSearch/InjectTTML.cs
@@ -27,11 +27,11 @@ namespace MediaButler.BaseProcess.AzureSearch
             string errorTxt = string.Format("[{0}] process Type {1} instance {2} has not compensation method", this.GetType().FullName, myRequest.ProcessTypeId, myRequest.ProcessInstanceId);
             Trace.TraceWarning(errorTxt);
         }
-        private void CreateVideoTextIndex(SearchServiceClient serviceClient)
+        private void CreateVideoTextIndex(SearchServiceClient serviceClient,string indexName)
         {
             var definition = new Microsoft.Azure.Search.Models.Index()
             {
-                Name = "videotxt",
+                Name = indexName,
                 Fields = new[]
                 {
                     new Field("id",DataType.String)                 { IsKey=true,IsRetrievable=true,IsFacetable=true},
@@ -47,6 +47,29 @@ namespace MediaButler.BaseProcess.AzureSearch
 
             serviceClient.Indexes.Create(definition);
         }
+        private ILocator CreateStreamingLocator(string outputAssetid)
+        {
+            IAssetFile assetFile = null;
+            ILocator locator = null;
+
+            var daysForWhichStreamingUrlIsActive = 365;
+            var outputAsset = _MediaServicesContext.Assets.Where(a => a.Id == outputAssetid).FirstOrDefault();
+
+            var accessPolicy = _MediaServicesContext.AccessPolicies.Create(
+                outputAsset.Name
+                , TimeSpan.FromDays(daysForWhichStreamingUrlIsActive)
+                , AccessPermissions.Read);
+            var assetFiles = outputAsset.AssetFiles.ToList();
+
+            assetFile = assetFiles.Where(f => f.Name.ToLower().EndsWith(".ism")).FirstOrDefault();
+            locator = _MediaServicesContext.Locators.CreateLocator(LocatorType.OnDemandOrigin, outputAsset, accessPolicy, DateTime.UtcNow.AddMinutes(-5));
+            //Add Smooth URL to Metadata
+            if (!myRequest.MetaData.ContainsKey("smoothurl"))
+            {
+                myRequest.MetaData.Add("smoothurl", locator.Path + assetFile.Name + "/manifest");
+            }
+            return locator;
+        }
         private void UploadTTML(SearchServiceClient serviceClient, string indexName, IAsset myAsset)
         {
             List<IndexAction<videotext>> data = new List<IndexAction<videotext>>();
@@ -55,6 +78,10 @@ namespace MediaButler.BaseProcess.AzureSearch
 
             IAssetFile myTTML = (from f in myAsset.AssetFiles select f).Where(f=>f.Name.EndsWith("ttml")).FirstOrDefault();
             ILocator locator = (from l in myAsset.Locators select l).Where(l => l.Type == LocatorType.OnDemandOrigin).FirstOrDefault();
+            if (locator==null)
+            {
+                locator = CreateStreamingLocator(myAsset.Id);
+            }
             IAssetFile ismfile = (from f in myAsset.AssetFiles select f).Where(f => f.Name.EndsWith("ism")).FirstOrDefault();
 
             XmlDocument xmlDoc = new XmlDocument();
@@ -126,7 +153,7 @@ namespace MediaButler.BaseProcess.AzureSearch
             if (!serviceClient.Indexes.Exists(indexName))
             {
                
-                CreateVideoTextIndex(serviceClient);
+                CreateVideoTextIndex(serviceClient,indexName);
             }
           
             IAsset asset = (from m in _MediaServicesContext.Assets select m).Where(m => m.Id == myRequest.AssetId).FirstOrDefault();
